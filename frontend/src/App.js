9ee97a68411708a53f1f86dc0e23a59158be0e89
IT21343216 <it21343216@my.sliit.lk>
2023-09-01 20:35:11 +0530

    UserManagementComplete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState, useEffect} from "react";
+import UserContext from "./components/ContextComponents/ContextComponent";
+
+
+//headers
+import UserHeader from './components/UserHeader/UserHeader';
+import DoctorHeader from './components/DoctorHeader/DoctorHeader';
+// import DoctorFooter from './components/DoctorFooter/DoctorFooter';
+
+
+//UserManagementSystem
+import Index from "./components/Index/Index";
+import Login from './components/Login/Login';
+import AddUser from './components/AddUser/AddUser';
+import PatientHome from './components/PatientHomePage/Phomepage';
+import DoctorHome from './components/DoctorHome/DoctorHomePage';
+import ViewProfile from './components/ViewUserDetails/ViewUserDetails';
+import UpdateUserDetails from './components/UpdateUserDetails/UpdateUserDetails';
+
+
 
 function App() {
+
+  // user details pass
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <UserContext.Provider value={{ user, setUser }}>
+      <div className="App">
+
+      {user?.UserType === 'Patient'? (
+          <UserHeader />
+        ) : (
+          <DoctorHeader />
+        )}
+        <Routes>
+
+          {/* user management */}
+          <Route path='' element={<Index />} />
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/add' element={<AddUser/>}/>
+          <Route path='/PatientHome/:id' element={<PatientHome/>} />
+          <Route path='/DoctorHome/:id' element={<DoctorHome/>}/>
+          <Route path='/profile/:id' element={<ViewProfile/>}/>
+          <Route path='/updateProfile/:id' element={<UpdateUserDetails/>}/>
+         
+
+        </Routes>
+        {/* {user?.UserType === 'Doctor' && <DoctorFooter />} */}
+
+      </div>
+     </UserContext.Provider>
+  </Router>
   );
 }
 
---
+++ frontend/src/components/DoctorHeader/DoctorHeader.js
@@ -0,0 +1,99 @@
+import React, {useContext} from "react";
+import { useLocation, useParams } from 'react-router-dom';
+import UserContext from "../ContextComponents/ContextComponent";
+
+function Header(props) {
+
+    const location = useLocation();
+    const params = useParams();
+    const id = props.id
+
+    const { user } = useContext(UserContext);
+    console.log(user)
+
+    //Log out function
+    function logOut() {
+        localStorage.clear();
+        window.location.href = `/login`
+    }
+
+    const hideHeader = location.pathname === '/' || location.pathname === '/login' || location.pathname === '/add';
+
+    if (hideHeader) {
+        return null; // Render nothing if header should be hidden
+    }
+
+    //Log out function
+    function logOut() {
+        localStorage.clear();
+    }
+
+    //Home pages Control
+    const HomepagesHandle = async () => {
+        if (user.Fullname) {
+            if (user.UserType === "Doctor") {
+                window.location.href = `/DoctorHome/${user._id}`;
+            }
+            // else if (user.UserType === "Doctor") {
+            //     // history(`/trainer_home/${user._id}`, { state: { id:user.Fullname } })
+            //     window.location.href = `/trainer_home/${user._id}`;
+            // }
+        }
+
+    }
+
+    return (
+
+        <nav className="navbar navbar-expand-lg" style={{ background: "#333333" }}>
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation" >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <b><a className="navbar-brand" onClick={HomepagesHandle} style={{ color: "#99FF33" }}>FitCrib</a></b>
+                {/* change */}
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/allusers" style={{ color: "#99FF33" }}>All Users</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/finance" style={{ color: "#99FF33" }}>Finance</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/addTransaction" style={{ color: "#99FF33" }}>Create a Transaction</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/viewTransaction" style={{ color: "#99FF33" }}>View Transactions</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/financialperformance" style={{ color: "#99FF33" }}>Financial Performance</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/calculate" style={{ color: "#99FF33" }}>Calculations</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/fnfaq" style={{ color: "#99FF33" }}>FAQs</a>
+                        </li>
+                        <li>
+                            <div className="NavigationBarBarDropdown dropdown">
+                                <button className="NavigationBarB btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" >
+                                    {user.Username}
+                                </button>
+
+                                <ul className="NavigationBarB dropdown-menu dropdown-menu-dark">
+                                    <li><a className="NavigationBarB dropdown-item" onClick={(e) => {
+                                        e.preventDefault();
+                                        window.location.href = `/profile/${user._id}`
+                                    }}>My Profile</a></li>
+                                    <li><a className="NavigationBarB dropdown-item" href="/login">Log out</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
